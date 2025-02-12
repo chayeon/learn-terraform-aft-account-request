438f81ba15562e90ecebcc55372edb67e473160b
EC2 Default User <ec2-user@ip-172-31-35-25.ap-northeast-2.compute.internal>
2025-02-12 05:52:33 +0000

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "sandbox02@chayeon.kr"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "cyhong1020@naver.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
